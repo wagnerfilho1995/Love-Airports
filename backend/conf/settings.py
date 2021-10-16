9dba3c714381f980a5abb238981385289822a008
wagnerfilho1995 <wwbff@ic.ufal.br>
2021-10-16 18:42:13 -0300

    hidding secret key

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -16,17 +16,22 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-64+hhv7!h0k)vdj$4f7_5xi7(i@j$s*#)%ebjcx@p_i_hj_g&e'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+try:
+    from .secret import SECRET_KEY, DEBUG, ALLOWED_HOSTS, DATABASES
+    # Criar um arquivo secret.py com estas informações
+except:
+    SECRET_KEY = 'SUA_CHAVE_SECRETA'
+    DEBUG = True
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'name_database', # Criar um banco Postgree com o nome que você botar aqui
+            'USER': 'user_database', # Seu usuário postgres
+            'HOST': 'localhost',
+            'PORT': 5432, #'', # 8000 is default
+        }
+    }
 
 # Application definition
 
@@ -77,22 +82,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'conf.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'airports',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '', # 8000 is default
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
